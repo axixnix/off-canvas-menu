d87eb3fd3199f1f925db1b65a7c89dab525fc310
axixnix <henryirons@yahoo.com>
2018-12-15 18:13:11 +0100

    just completed the off-site canvas menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 // START EDITING YOUR JAVASCRIPT HERE
 // ===============
 const button = document.querySelector('.menu-button')
+
+button.addEventListener('click', push)
+
+function push (e) {
+  const bodyClass = document.body.classList
+  if (bodyClass.contains('offsite-is-open')) {
+    bodyClass.remove('offsite-is-open')
+  }else {bodyClass.add('offsite-is-open')}
+}
+
+/**alternatively
+ * button.addEventlistener('click',e => document.body.classList.toggle('offsite-is-open'))
+ */
